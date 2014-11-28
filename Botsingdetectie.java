c05a02a36fb2a70c09443d5dffdf641191d478eb
aareschluchtje <remcosannen@gmail.com>
2014-11-28 14:20:47 +0100

    Voelsprieten bijgevoegd
    Botsingdetectie aangepast en voelsprieten toegevoegd.

--- Botsingdetectie.java
+++ Botsingdetectie.java
@@ -2,19 +2,28 @@ package boebot;
 public class Botsingdetectie{
   private IrDetectie irL;
   private IrDetectie irR;
+  private VsDetectie vsL;
+  private VsDetectie vsR;
   private boolean IrDetectL;
   private boolean IrDetectR;
+  private boolean VsDetectL;
+  private boolean VsDetectR;
 
   public Botsingdetectie(){
        irL = new IrDetectie(8, 9);
        irR = new IrDetectie(2, 0);
+       vsL = new VsDetectie(8, 9);
+       vsR = new VsDetectie(2, 0);
   }
 
   public boolean detectEdge(){
 
     IrDetectL = irL.detect();
     IrDetectR = irR.detect();
-    if(IrDetectL || IrDetectR){
+    VsDetectL = vsL.detect();
+    VsDetectR = vsR.detect();
+
+    if(IrDetectL || IrDetectR || vsDetectL || vsDetectR){
         return true;
 
     }else{
---
+++ VsDetectie.java
@@ -0,0 +1,21 @@
+package boebot;
+import stamp.core.*;
+
+public class VsDetectie{
+  boolean vsDetect;
+  private int pinRead;
+  private int pinSend;
+
+ public VsDetectie(int pinR){
+     pinRead = pinR;
+
+ }
+ public void setPinRead(int pin){
+   pinRead = pin;
+ }
+ public boolean detect(){
+    vsDetect = CPU.readPin(CPU.pins[pinRead]);
+    return  !vsDetect;
+ }
+
+}
